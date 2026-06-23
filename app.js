1fecf95a258bbd95cc0c17b14ce7496450523ef8
Basel Mohsen <baselmohsen585@gmail.com>
2026-06-22 17:52:15 -0700

    fix: remove runner logs and ignore build filess

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     </head>
     <body>
         <div class="card">
-            <h1>🚀 DevOps Demo App the last version 55 this is me basel mohsen zaki</h1>
+            <h1>🚀 DevOps Demo App the last version  this is me basel mohsen zaki 🚀 🚀 🚀 🚀 </h1>
             <p><strong>Hostname:</strong> ${os.hostname()}</p>
             <p><strong>Platform:</strong> ${os.platform()}</p>
             <p><strong>Status:</strong> ✅ Running</p>
